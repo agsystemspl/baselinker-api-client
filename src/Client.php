94bccd09dada1eea9307a3e33dbe13d619364196
Paweł Feruś <panther.mindc@gmail.com>
2018-12-23 01:25:30 +0100

    phpdoc with API methods

--- src/Client.php
+++ src/Client.php
@@ -2,6 +2,42 @@
 
 namespace AGSystems\Baselinker\API;
 
+/**
+ * @method mixed addCategory(array $parameters)
+ * @method mixed addProduct(array $parameters)
+ * @method mixed addProductVariant(array $parameters)
+ * @method mixed deleteCategory(array $parameters)
+ * @method mixed deleteProduct(array $parameters)
+ * @method mixed deleteProductVariant(array $parameters)
+ * @method mixed getCategories(array $parameters)
+ * @method mixed getProductsData(array $parameters)
+ * @method mixed getProductsList(array $parameters)
+ * @method mixed getStoragesList()
+ * @method mixed updateProductsQuantity(array $parameters)
+ * @method mixed updateProductsPrices(array $parameters)
+ *
+ * @method mixed getJournalList(array $parameters)
+ * @method mixed addOrder(array $parameters)
+ * @method mixed getOrders(array $parameters)
+ * @method mixed getOrdersByEmail(array $parameters)
+ * @method mixed getInvoices(array $parameters)
+ * @method mixed getOrderStatusList(array $parameters)
+ * @method mixed getNewReceipts(array $parameters)
+ * @method mixed setOrderFields(array $parameters)
+ * @method mixed setOrderPayment(array $parameters)
+ * @method mixed setOrderStatus(array $parameters)
+ * @method mixed setOrderReceipt(array $parameters)
+ *
+ * @method mixed createPackage(array $parameters)
+ * @method mixed createPackageManual(array $parameters)
+ * @method mixed getCouriersList()
+ * @method mixed getCourierFields(array $parameters)
+ * @method mixed getCourierServices(array $parameters)
+ * @method mixed getCourierAccounts(array $parameters)
+ * @method mixed getLabel(array $parameters)
+ * @method mixed getOrderPackages(array $parameters)
+ * @method mixed getCourierPackagesStatusHistory
+ */
 class Client
 {
     protected $connector = 'https://api.baselinker.com/connector.php';
